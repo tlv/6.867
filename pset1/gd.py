55c43d58a145e034c2116598429f071befd9861c
tlv <tlv@github.com>
2015-09-27 22:05:05 -0400

    gradient v0

--- pset1/gd.py
+++ pset1/gd.py
@@ -1,8 +1,18 @@
 import numpy as np
-ep = 0.0000001
+ep = 0.000001
 
 def gd(f, start, step):
+  pass
 
 def gradient(f, start):
-  
-  
+  start = start.astype(float)
+  n = start.shape[0]
+  plus = np.identity(n) * ep
+  minus = np.identity(n) * (-ep)
+  startd1 = np.tile(start, (n, 1))
+  startd1 += plus
+  startd2 = np.tile(start, (n, 1))
+  startd2 += minus
+  plusv = np.array([f(startd1[i,:]) for i in range(n)])
+  minusv = np.array([f(startd2[i,:]) for i in range(n)])
+  return 1/(2*ep) * (plusv - minusv)
