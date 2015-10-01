33a3e9e9de67bd5607b9caacbfa44cb53b3a7732
tlv <tlv@github.com>
2015-09-30 20:49:21 -0400

    partial 4.1

---
+++ pset1/lad.py
@@ -0,0 +1,35 @@
+import numpy as np
+import gd
+import lbfr
+import parker_rr as rr
+from hw1_res import homework1
+
+
+def compute_lad(Phi, Theta, Y, lam=0):
+  result = 0
+  result += np.dot(np.ones(Y.size), np.absolute(np.dot(Phi, Theta) - Y))
+  result += lam * np.linalg.norm(Theta)**2
+  return result
+
+
+def obj_func(Phi, Y, lam=0):
+  f = lambda x: compute_lad(Phi, x, Y, lam=lam)
+  return f
+
+
+data_train = homework1.getData('regress_train.txt')
+data_cv = homework1.getData('regress_validate.txt')
+data_test = homework1.getData('regress_test.txt')
+
+X_train = data_train[0].reshape(data_train[0].size)
+Y_train = data_train[1].reshape(data_train[1].size)
+X_cv = data_cv[0].reshape(data_cv[0].size)
+Y_cv = data_cv[1].reshape(data_cv[1].size)
+X_test = data_test[0].reshape(data_test[0].size)
+Y_test = data_test[1].reshape(data_test[1].size)
+
+mins = {}
+for M in range(10):
+  for lam in [2 ** x for x in np.arange(-3, 6, 0.5)]:
+    f = obj_func(lbfr.compute_Phi_poly(X_train, M), Y_train, lam)
+    
